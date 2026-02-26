551c452a01ffee0aac4815fcf42cb220bc9ebc6e
Alex <ahuberde@my.centennialcollege.ca>
2026-02-26 12:05:06 -0600

    created file for midterm

---
+++ midterm/Source/midterm/midterm.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "midterm.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, midterm, "midterm" );
